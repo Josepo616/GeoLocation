22888fceee93129e60340832c384a03bb59898fe
José Álvarez <josemejia61976751@gmail.com>
2025-09-01 11:09:41 -0600

    chore: init

---
+++ GeoLocation/GeoLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoLocationApp.swift
+//  GeoLocation
+//
+//  Created by JoseAlvarez on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct GeoLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GeoLocationTests/GeoLocationTests.swift
@@ -0,0 +1,17 @@
+//
+//  GeoLocationTests.swift
+//  GeoLocationTests
+//
+//  Created by JoseAlvarez on 9/1/25.
+//
+
+import Testing
+@testable import GeoLocation
+
+struct GeoLocationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
